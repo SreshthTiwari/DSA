ef9400e20ad30e051d960a1ff41caaafd92850f3
Sreshth <sreshthtiwari13@gmail.com>
2024-06-24 19:43:56 +0530

    last push for day

--- 2406.py
+++ 2406.py
@@ -1,1 +1,12 @@
-print("hello")
+# find max normal
+
+def findmax(seq):
+    max_number = seq[0]
+    for num in seq:
+        if num>max_number:
+            max_number=num
+    return max_number
+
+print(findmax([1, 2, 3,45, 4]))
+
+# Runtime --> amount of primitive operations required to solve problem
